b633425825fc5be5b118a566f2be922a2d51e865
md-golam-kibriya <universesoftcare@gmail.com>
2023-11-25 20:05:11 +0600

    global error handle

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { UserRoute } from './app/modules/user/user.route';
 
@@ -8,5 +8,24 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/users', UserRoute);
+app.get('/', (req: Request, res: Response) => {
+
+    res.send("route is working")
+});
+
+app.all("*", (req: Request, res: Response) => {
+    res.status(404).json({
+        "success": false,
+        "message": "API not found",
+        "error": {
+            "code": 404,
+            "description": "API not found!"
+        }
+    });
+});
+
+app.use((req: Request, res: Response) => {
+    return res.status(400).json({ success: false, error: "Not a valid routes" });
+});
 
 export default app;
